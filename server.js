424da263693b7f57c1d980d6ed44effd98875580
Omolara5861 <debbiegterra@gmail.com>
2022-07-02 03:06:47 +0100

    project psudo code

---
+++ server.js
@@ -0,0 +1,11 @@
+/**
+ * Create an express server
+    Connect to mongoDB
+    Initialize Express
+    Initialize Express Middleware
+    create a simple get request route
+    Inject route
+    Listen to app
+ */
+
+
