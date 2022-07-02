a19ea39ce01429037cd8d26be24f258aba86fe5d
Omolara5861 <debbiegterra@gmail.com>
2022-07-02 03:19:04 +0100

    server setup

--- server.js
+++ server.js
@@ -8,4 +8,24 @@
     Listen to app
  */
 
+    const express = require('express');
+    const connectDB = ('./db');
+    require('dotenv').config();
+    const {PORT} = process.env;
 
+
+    // Connect to database
+    connectDB();
+
+    // Initialize Express
+    const app = express();
+
+    // Initialize Express Middleware
+    app.use(express.json({extended: false}));
+
+    const port = process.env.PORT || PORT;
+
+    app.get('/', (req, res) => {
+       res.json({msg: `Hello Express Server`});
+    });
+    app.listen(port, () => console.log(`listening on http://localhost:${port}`));
